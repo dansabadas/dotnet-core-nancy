31ea2a01037477fb680db535882c2be8f948ad47
DESKTOP-8OGCHC2\dan_s <dan.sabadas@gmail.com>
2018-04-20 22:03:59 +0300

    fixes for project json

--- Hello-Microservices/Startup.cs
+++ Hello-Microservices/Startup.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Hello_Microservices
 {
-    public class Startup
+  public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940

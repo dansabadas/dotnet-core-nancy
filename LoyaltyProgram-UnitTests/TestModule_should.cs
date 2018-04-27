350a4fed0378896d0970154b34f78b4e84f92224
z003p8vs <z003p8vs@ROCLUG1201NB>
2018-04-27 17:23:04 +0300

    added the unit test project

---
+++ LoyaltyProgram-UnitTests/TestModule_should.cs
@@ -0,0 +1,28 @@
+using Nancy;
+using Nancy.Testing;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LoyaltyProgram_UnitTests
+{
+  public class TestModule_should
+  {
+    public class TestModule : NancyModule
+    {
+      public TestModule()
+      {
+        Get("/", _ => 200);
+      }
+    }
+
+    [Fact]
+    public async Task respond_ok_to_request_to_root_Async()
+    {
+      var sut = new Browser(with => with.Module<TestModule>());
+      var actual = await sut.Get("/");
+
+      Assert.Equal(HttpStatusCode.OK, actual.StatusCode);
+    }
+  }
+}
